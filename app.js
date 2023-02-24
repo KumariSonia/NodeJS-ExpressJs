bd72da96ae6b1d11cb677b46ffab19dea784a153
milan bamaniya <milan@fliptree.com>
2023-02-24 08:38:28 +0530

    created external module Calc and using it

--- app.js
+++ app.js
@@ -3,9 +3,12 @@
 // var c=a+b;
 // console.log("a+b="+c)
 var http = require('http')
+var calc = require('./Calc.js')
+var add = calc.add(2, 5)
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.end("Welcome back to server")
+    res.end("2+5 =" + add)
 }
 ).listen(8080)
+
