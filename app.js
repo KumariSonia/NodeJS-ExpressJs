6ff87ccd6622b9d307db7a1b56229d7251c400f6
milan bamaniya <milan@fliptree.com>
2023-02-24 08:56:42 +0530

    Created example for file system

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-// var a= 9;
-// var b=8;
-// var c=a+b;
-// console.log("a+b="+c)
 var http = require('http')
-var calc = require('./Calc.js')
-var add = calc.add(2, 5)
+var fs = require('fs')
+
+var fileData;
+//reading file
+fs.readFile('Calc.js', 'utf8', function (err, data) {
+    fileData = data
+})
+
+//for creating file
+fs.writeFile('Calc1.js', 'console.log("done")', function (err) {
+    console.log("file saved")
+})
+
+//for appending file
+fs.appendFile('Calc1.js', 'console.log("done")',function (err) {
+    console.log("file appended")
+})
+
+//for deleting file
+fs.unlink('Calc1.js',function(err)
+{
+    console.log("Deleted")
+})
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.end("2+5 =" + add)
+    res.end(fileData)
 }
 ).listen(8080)
-
