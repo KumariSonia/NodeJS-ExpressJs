b8bcafb272a60b19ffbc9d15bf5bb8d7f5f1636c
milan bamaniya <milan@fliptree.com>
2023-02-24 07:53:09 +0530

    Added server configuration for nodeJS

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-var a= 9;
-var b=8;
-var c=a+b;
-console.log("a+b="+c)
+// var a= 9;
+// var b=8;
+// var c=a+b;
+// console.log("a+b="+c)
+var http = require('http')
+
+http.createServer(function(req,res){
+    res.write("Welcome back to server")
+    res.end()
+}
+).listen(8080)
